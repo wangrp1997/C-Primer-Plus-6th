6d81e39599544d424fbc4c1f82ba9b680c1c39ca
Robert <robert@Roberts-MacBook-Air.local>
2021-04-21 19:36:00 +0800

    1

--- ch1/p67.c
+++ ch1/p67.c
@@ -1,2 +1,12 @@
 //test
-//this is test2
+#include <stdio.h>
+
+int main(void)
+{
+    int num;
+    num = 1;
+    printf("I am a simple");
+    printf("computer.\n");
+    printf("My favorite number is %d because it is first.\n", num);
+    return 0;
+}
